124e569c43fd1768ea22f1de6894621a30c4ba12
ErVaaL <ErVaLofficial@protonmail.com>
2025-12-28 20:33:20 +0100

    Dockerized the app and implemented gateway (traefik)

--- process-bootstrap/src/main/java/com/erval/argos/process/bootstrap/web/ProcessController.java
+++ process-bootstrap/src/main/java/com/erval/argos/process/bootstrap/web/ProcessController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/process")
 @RequiredArgsConstructor
 public class ProcessController {
 
